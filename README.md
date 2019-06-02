b6665b58bbed9c5004ce662128e43e56bb3ea79d
Eduardo Miguel Fírvida Donéstevez <efirvida@gmail.com>
2019-06-02 16:29:24 +0200

    the commit

---
+++ README.md
@@ -0,0 +1,5 @@
+The names is:
+=========================
+
+    git clone git@github.com:efirvida/the-name.git
+    ./the_name.py
---
+++ the_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import glob
+import collections
+
+lists = glob.glob("*.txt")
+names = []
+for name_list in lists:
+    with file(name_list,'r') as _list:
+        names += [name.strip() for name in _list.readlines()]
+
+counter=collections.Counter(names)
+names_counter = sorted(counter.items(), key=lambda item: item[1], reverse=True)
+winner = names_counter[0][0]
+clasifications = "\n".join([str(j) + " -> " + str(i) for i,j in names_counter])
+print("And the winner is: " + winner + " ehhhhhhhhh!!!!!")
+print(clasifications)
+
