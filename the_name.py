6e744f77f61f1be24489a331a37558eb453597d3
Eduardo Miguel Fírvida Donéstevez <efirvida@gmail.com>
2019-06-03 14:08:28 +0200

    one's bug

--- the_name.py
+++ the_name.py
@@ -8,7 +8,7 @@ lists = glob.glob("*.txt")
 names = []
 for name_list in lists:
     with open(name_list,'r') as _list:
-        names += [name.strip() for name in _list.readlines()]
+        names += set([name.strip() for name in _list.readlines()])
 
 counter=collections.Counter(names)
 names_counter = sorted(counter.items(), key=lambda item: item[1], reverse=True)
